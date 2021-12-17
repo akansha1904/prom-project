e3ee156e5aa59a9a3399ca9075f5e33eab680e63
akansha immanuel <44111918+akansha1904@users.noreply.github.com>
2021-12-17 14:28:00 +0530

    Update python-app.py

--- python/python-app.py
+++ python/python-app.py
@@ -1,35 +1,76 @@
-import http.server
-import os
-from prometheus_client import start_http_server, Counter , Gauge
 
+from prometheus_client import start_http_server, Counter , Gauge
+import os
+import sys
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))))
+
+import threading
+from http.server import HTTPServer
+import socket
+import time
+
+
+from prometheus.registry import Registry
+from prometheus.exporter import PrometheusMetricHandler
+import psutil
 
 REQUEST_COUNT = Counter('app_requests_count', 'total app http request count',['app_name', 'endpoint'])
 RANDOM_COUNT = Counter('app_random_count','increment counter by random value')
 ram_metric = Gauge("pod_memory_usage_bytes", "Memory usage in bytes.")
 cpu_metric = Gauge("pod_cpu_usage_percent", "CPU usage percent.")
-APP_PORT = 9371
-METRICS_PORT = 8371
+PORT_NUMBER = 8371
 
-class HandleRequests(http.server.BaseHTTPRequestHandler):
 
-    def do_GET(self):
-        
-        MEM=str(os.popen('cat /sys/fs/cgroup/memory/memory.usage_in_bytes').readlines())
-        CPU=str(os.popen('cat /sys/fs/cgroup/cpuacct/cpuacct.usage').readlines())
+def gather_data(registry):
+    """Gathers the metrics"""
 
-        self.send_response(200)
-        self.send_header("Content-type", "text/html")
-        self.end_headers()
-        self.wfile.write(bytes("<html><head><title>First Application</title></head><body style='color: #333; margin-top: 30px;'><center><h2>Welcome to our first Prometheus-Python application.</center></h2></body></html>", "utf-8"))
-        self.wfile.close()
-        REQUEST_COUNT.labels('prom_python_app', self.path).inc()
-        RANDOM_COUNT.inc()
-        ram_metric.set(int(MEM[2:-4])) 
-        cpu_metric.set(int(CPU[2:-4]))
+    # Get the host name of the machine
+    host = socket.gethostname()
 
-        
+    # Create our collectors
+    ram_metric = Gauge("memory_usage_bytes", "Memory usage in bytes.",
+                       {'host': host})
+    cpu_metric = Gauge("cpu_usage_percent", "CPU usage percent.",
+                       {'host': host})
+
+    # register the metric collectors
+    registry.register(ram_metric)
+    registry.register(cpu_metric)
+
+    # Start gathering metrics every second
+    while True:
+        time.sleep(1)
+
+        # Add ram metrics
+        ram = psutil.virtual_memory()
+        swap = psutil.swap_memory()
+
+        ram_metric.set({'type': "virtual", }, ram.used)
+        ram_metric.set({'type': "virtual", 'status': "cached"}, ram.cached)
+        ram_metric.set({'type': "swap"}, swap.used)
+
+        # Add cpu metrics
+        for c, p in enumerate(psutil.cpu_percent(interval=1, percpu=True)):
+            cpu_metric.set({'core': c}, p)
 
 if __name__ == "__main__":
-    start_http_server(METRICS_PORT)
-    server = http.server.HTTPServer(( '', APP_PORT), HandleRequests)
-    server.serve_forever()
+
+    # Create the registry
+    registry = Registry()
+
+    # Create the thread that gathers the data while we serve it
+    thread = threading.Thread(target=gather_data, args=(registry, ))
+    thread.start()
+
+    # Set a server to export (expose to prometheus) the data (in a thread)
+    try:
+        # We make this to set the registry in the handler
+        def handler(*args, **kwargs):
+            PrometheusMetricHandler(registry, *args, **kwargs)
+
+        server = HTTPServer(('', PORT_NUMBER), handler)
+        server.serve_forever()
+
+    except KeyboardInterrupt:
+        server.socket.close()
+        thread.join()
